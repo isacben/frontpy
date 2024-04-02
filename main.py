661495459143a19f0397c4251eaa6840deeeead5
Isaac Sandoval <isaac.sandoval@airwallex.com>
2024-04-02 16:07:33 -0700

    playing a bit

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def tag(elemen: str, content: str) -> str:
-    html = f'<{elemen}>{content}</{elemen}>'
+SIMPLE_TAGS = ["canvas", "h1", "h2", "h3", "p", "a", "div", "span", "select"]
+
+def tag(name, *children) -> str:
+    html = f'<{name}' 
+
+    for child in children:
+        if type(child) == str:
+            html += '>' + child
+        else:
+            html += f' {child}>'
+
+    html += f'</{name}>'
+
     return html
+
+
+
 
 
 def div(children: str, props: str) -> str:
@@ -10,15 +24,15 @@ def div(children: str, props: str) -> str:
         </div>'''
 
 
-def h1(children: str):
-    return tag('h1', children)
+def h1(*children):
+    return tag('h1', *children)
 
+def div(*children):
+    return tag('div', *children)
 
 def main():
-    html = div(
-               h1('hello world'), 'class="test-class"'
-           )
-    print(html)
+    print(div(h1('Hello world')))
+    # print(p('hello world'))
 
 
 if __name__ == "__main__":
